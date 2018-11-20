da01d33188ab69c90594144f7595a16c2c0fcf84
中山一輝 <nakayama@MacBook-Home.local>
2018-11-20 22:54:57 +0900

    手の先の検出

--- RecogHand/main.cpp
+++ RecogHand/main.cpp
@@ -7,9 +7,23 @@
 //
 
 #include <opencv2/opencv.hpp>
+#include <math.h>
 
+void swapIfBigger(std::pair<int, double>& a, std::pair<int, double>& b) {
+    if (a.second < b.second) {
+       std::swap(a, b);
+    }
+}
 
-void extractSkin(const cv::Mat& frame, cv::Mat& dst) {
+const std::pair<int, double>& min(const std::pair<int, double>& a, const std::pair<int, double>& b) {
+    if (a.second < b.second) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+static void extractSkin(const cv::Mat& frame, cv::Mat& dst) {
     cv::Mat mask;
     cv::Mat hsv;
     
@@ -25,6 +39,14 @@ void extractSkin(const cv::Mat& frame, cv::Mat& dst) {
     
     // 肌色の部分のみ255にする
     cv::bitwise_and(whiteImage, whiteImage, dst, mask);
+}
+
+static float calcThreePointAngle(const cv::Point& a, const cv::Point& b, const cv::Point& c) {
+#define rad_to_deg(rad) (((rad)/2/M_PI)*360)
+    
+    float rad1 = 2 * M_PI + atan2(b.y - a.y, a.x - b.x);
+    float rad2 = 2 * M_PI + atan2(b.y - c.y, c.x - b.x);
+    return rad_to_deg(rad1 - rad2);
 }
 
 void doProcess(const cv::Mat& frame, cv::Mat& rgb) {
@@ -45,8 +67,45 @@ void doProcess(const cv::Mat& frame, cv::Mat& rgb) {
 
     rgb = frame.clone();
     
+    // 面積の大きい上位２つの輪郭を選ぶ
+    std::pair<int, double> areaRanking[2] = {{-1,0}, {-1, 0}};
     for (int i = 0; i < contours.size(); i++){
-        cv::drawContours(rgb, contours, i, CV_RGB(0, 0, 255), 4);
+        const std::vector<cv::Point>& points = contours[i];
+        
+        double area = cv::contourArea(points);
+        std::pair<int, double> value(i, area);
+        
+        swapIfBigger(areaRanking[0], value);
+        swapIfBigger(areaRanking[1], value);
+    }
+    
+    // 輪郭線の描画
+    cv::drawContours(rgb, contours, areaRanking[0].first, cv::Scalar(255, 0, 0), 2);
+    cv::drawContours(rgb, contours, areaRanking[1].first, cv::Scalar(255, 255, 0), 2);
+
+    // 面積の上位２つのうち、どちらが手かを判別する
+    const int span = 100;
+    for (int i = 0; i < 2 && areaRanking[i].first >= 0; i++) {
+        const std::vector<cv::Point>& points = contours[areaRanking[i].first];
+        
+        for (int start = 0; start < points.size();) {
+            int cindex = (start + (span / 2)) % points.size();
+            int eindex = (start + span) % points.size();
+            
+            cv::Point s = points[start];
+            cv::Point c = points[cindex];
+            cv::Point e = points[eindex];
+            
+            float angle = calcThreePointAngle(s, c, e);
+            if (0 <= angle && angle < 75) {
+                cv::line(rgb, s, c, cv::Scalar(0, 0, 255), 2);
+                cv::line(rgb, c, e, cv::Scalar(0, 0, 255), 2);
+                cv::circle(rgb, c, 5, cv::Scalar(255, 255, 0), -1);
+                start += (span / 2);
+            } else {
+                start++;
+            }
+        }
     }
 }
 
@@ -75,6 +134,5 @@ int main(int argc, const char * argv[]) {
             break;
         }
     }
-    
     return 0;
 }
